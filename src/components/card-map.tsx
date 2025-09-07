37136e6bcae6978ecedc8e8fd4307e4f2958c6cb
Jan Blazej <jan.blazej@footshop.cz>
2025-09-07 23:03:16 +0200

    card border gradient

--- src/components/card-map.tsx
+++ src/components/card-map.tsx
@@ -2,11 +2,9 @@ import {EntryMap} from '@/db'
 
 export const CardMap = ({area}: EntryMap) => (
   <div
-    className="w-full shrink-0 flex h-full contain-intrinsic overflow-hidden bg-card-surface rounded-2xl border border-solid border-white/15"
+    className="w-full shrink-0 flex h-full contain-intrinsic overflow-hidden card-border-gradient"
     style={{gridArea: area, transformStyle: 'preserve-3d'}}
   >
-    <div className="flex flex-col w-full grow overflow-hidden relative z-40 items-center justify-center rounded-2xl">
-      {area}
-    </div>
+    <div className="flex flex-col w-full grow overflow-hidden h-full items-center justify-center">{area}</div>
   </div>
 )
