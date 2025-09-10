3209a26b8238b559cd9a788d03d023ea2f3b126e
Jan Blazej <jan.blazej@footshop.cz>
2025-09-10 19:43:06 +0200

    add cv

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,6 +1,6 @@
 import {CV} from '@/components'
 
-export default async function Home() {
+export default async function Cv() {
   return (
     <div className="flex w-full h-full justify-center overflow-y-scroll pt-[116px]">
       <div className="max-w-[700px] h-full flex flex-col items-center py-[56px] px-[64px]">
