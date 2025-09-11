7909e3c8ff0fccb3dfe1a28a8b2be020597df846
Jan Blazej <jan.blazej@footshop.cz>
2025-09-11 09:19:58 +0200

    Add video icon

--- src/components/card-shot.tsx
+++ src/components/card-shot.tsx
@@ -36,8 +36,15 @@ export const CardShot: FC<EntryShot> = ({area, properties, title, description, i
               }}
             />
             {videos && (
-              <div className="absolute inset-0 bg-green-800 rounded-2xl overflow-hidden">
+              <div className="absolute inset-0 rounded-2xl overflow-hidden">
                 <HoverVideo srcMp4={videos.mp4} srcWebm={videos.webm} poster={image} muted loop preload="auto" />
+              </div>
+            )}
+            {videos && (
+              <div className="absolute right-[12px] top-[12.47px] rounded-2xl overflow-hidden">
+                <div className="w-[22px] h-[22px] rounded-full flex items-center justify-center bg-white/15">
+                  <Image src="/svg/video.svg" alt="video-icon" width={12} height={12} />
+                </div>
               </div>
             )}
           </div>
